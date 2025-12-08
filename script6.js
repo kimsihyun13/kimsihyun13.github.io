1999c71be2c7872458906041025dc63af179323f
kimsihyun13 <jaikon.kimi@gmail.com>
2025-12-08 22:15:25 +0900

    Update script6.js

--- script6.js
+++ script6.js
@@ -1,7 +1,20 @@
-let angle = 0;
-
 function hello6(event) {
-    angle += 360;
-    event.target.style.transform = `rotate(${angle}deg)`;
-    event.target.style.transition = "0.5s";
+    let btn = event.target;
+
+
+    if (!btn.dataset.rotated) {
+        btn.dataset.rotated = "0";
+    }
+
+
+    if (btn.dataset.rotated === "0") {
+        btn.style.transform = "rotate(360deg)";
+        btn.dataset.rotated = "1";
+    } else {
+        btn.style.transform = "rotate(0deg)";
+        btn.dataset.rotated = "0";
+    }
+
+    btn.style.transition = "0.5s";
 }
+
