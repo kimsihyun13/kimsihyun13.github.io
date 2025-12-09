47786c306f59115ac8c8631ec0115c95207f3006
kimsihyun13 <jaikon.kimi@gmail.com>
2025-12-09 16:16:38 +0900

    Delete script4.js

--- script4.js
+++
@@ -1,9 +0,0 @@
-function hello4() {
-    let msg = document.getElementById("msg4");
-
-    if (msg.innerText === "") {
-        msg.innerText = "가";  
-    } else {
-        msg.innerText = "";    
-    }
-}
