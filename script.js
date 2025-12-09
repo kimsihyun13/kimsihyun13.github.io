2f031e17886bccafa3fc0d0653676a30f3583400
kimsihyun13 <jaikon.kimi@gmail.com>
2025-12-09 16:16:32 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function hello() {
-    let msg = document.getElementById("msg1");
-
-    if (msg.innerText === "") {
-        msg.innerText = "현";   
-    } else {
-        msg.innerText = "";    
-    }
-}
