f1ba3c200355d9e82c17fc346ae962269ee81fbe
kimsihyun13 <jaikon.kimi@gmail.com>
2026-02-13 13:29:50 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function hello() {
     let msg = document.getElementById("msg1");
 
     if (msg.innerText === "") {
-        msg.innerText = "현";   
+        msg.innerText = "살다 살다 별별 사람을 다 보네.";   
     } else {
         msg.innerText = "";    
     }
