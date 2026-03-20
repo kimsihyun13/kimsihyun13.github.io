43b224e65fb55911fd76b7a4c3d7ffc487ad91e9
kimsihyun13 <jaikon.kimi@gmail.com>
2026-03-20 19:18:56 +0900

    Delete script2.js

--- js/script2.js
+++
@@ -1,9 +0,0 @@
-function hello2() {
-    let msg = document.getElementById("msg2");
-
-    if (msg.innerText === "") {
-        msg.innerText = "는";   
-    } else {
-        msg.innerText = "";   
-    }
-}
