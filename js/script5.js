71c947b2af7d375b2497b7f8ebfcb9ea36808bb7
kimsihyun13 <jaikon.kimi@gmail.com>
2026-03-20 19:18:51 +0900

    Delete script5.js

--- js/script5.js
+++
@@ -1,9 +0,0 @@
-function hello5() {
-    let msg = document.getElementById("msg5");
-
-    if (msg.innerText === "") {
-        msg.innerText = "려";  
-    } else {
-        msg.innerText = "";    
-    }
-}
