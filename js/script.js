4708d65f5d92e952e76060ecf749d44fa8a3eb30
kimsihyun13 <jaikon.kimi@gmail.com>
2026-03-20 19:18:58 +0900

    Delete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function hello() {
-    let msg = document.getElementById("msg1");
-
-    if (msg.innerText === "") {
-        msg.innerText = "살다 살다 별별 사람을 다 보네.";   
-    } else {
-        msg.innerText = "";    
-    }
-}
